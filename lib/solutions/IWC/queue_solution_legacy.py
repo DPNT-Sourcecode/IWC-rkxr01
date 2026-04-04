2f428e7e8b5172b08bb15451388997a5a2a50f42
sbx_user1051 <sbx_user1051@169.254.2.217>
2026-04-04 17:49:23 +0000

    Sat Apr 04 17:49:23 UTC 2026

--- lib/solutions/IWC/queue_solution_legacy.py
+++ lib/solutions/IWC/queue_solution_legacy.py
@@ -100,6 +100,10 @@ class Queue:
             metadata.setdefault("group_earliest_timestamp", MAX_TIMESTAMP)
             new_tasks[(task.provider, task.user_id)] = task
 
+        if self.size == 0:
+            self._queue.extend(tasks)
+            return self._size
+
         for idx, existing_task in enumerate(self._queue):
             existing_task_id = (existing_task.provider, existing_task.user_id)
             if existing_task_id in new_tasks:
@@ -108,6 +112,7 @@ class Queue:
                     self._queue.append(new_tasks[existing_task_id])
                 else:
                     continue
+        print(self._queue)
         return self.size
 
     def dequeue(self):
@@ -252,3 +257,4 @@ async def queue_worker():
         logger.info(f"Finished task: {task}")
 ```
 """
+
